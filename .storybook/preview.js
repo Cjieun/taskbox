298161d181f9845d0a1fe7db46c5e978c0063a7a
jieun <122079153+Cjieun@users.noreply.github.com>
2023-10-07 01:14:56 +0900

    fifth commit

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,23 @@
 /** @type { import('@storybook/react').Preview } */
 import "../src/index.css";
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+// Registers the msw addon
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+// Initialize MSW
+initialize();
+
+const preview = {
+  decorators: [mswDecorator],
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
     },
   },
 };
+
+export default preview;
